de59840ab34bbe0d3d5ebe442e5423bfecf549ae
Faisal Alagla <alagla.faisal@gmail.com>
2023-04-15 17:12:41 +0300

    Using TagHelpers & client-side validations

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -12,11 +12,19 @@ namespace ServiceContracts.DTO
 	{
 		[Required(ErrorMessage = "Person Name can't be blank")]
 		public string? PersonName { get; set; }
+
 		[Required(ErrorMessage = "Email can't be blank")]
 		[EmailAddress(ErrorMessage = "Invalid Email")]
+		[DataType(DataType.EmailAddress)]
 		public string? Email { get; set; }
+
+		[DataType(DataType.Date)]
 		public DateTime? DateOfBirth { get; set; }
+
+		[Required(ErrorMessage = "A gender must be selected")]
 		public GenderOptions? Gender { get; set; }
+
+		[Required(ErrorMessage = "A country must be selected")]
 		public Guid? CountryID { get; set; }
 		public string? Address { get; set; }
 		public bool ReceiveNewsLetters { get; set; }
