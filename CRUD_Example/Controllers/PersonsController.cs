4d4ce4f2c2b0f0504b386244477cc9f2178cd01f
Faisal Alagla <alagla.faisal@gmail.com>
2023-05-05 10:40:38 +0300

    Generate PDF

--- CRUD_Example/Controllers/PersonsController.cs
+++ CRUD_Example/Controllers/PersonsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Rotativa.AspNetCore;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
@@ -151,6 +152,19 @@ namespace CRUD_Example.Controllers
 			await _personsService.DeletePerson(personUpdateRequest.PersonID);
 
 			return RedirectToAction("Index");
+		}
+
+		[Route("PersonsPDF")]
+		public async Task<IActionResult> PersonsPDF()
+		{
+			List<PersonResponse> persons = await _personsService.GetAllPersons();
+
+			//this class comes from the Rotativa package
+			return new ViewAsPdf("PersonsPDF", persons, ViewData)
+			{
+				PageMargins = new Rotativa.AspNetCore.Options.Margins() { Top = 20, Right = 20, Bottom = 20, Left = 20 },
+				PageOrientation = Rotativa.AspNetCore.Options.Orientation.Landscape
+			};
 		}
 	}
 }
